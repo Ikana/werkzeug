ecc904045bf12af4c9ad562d56d410c8d84e89fa
Rodrigo Quezada <rodrigo.quezada@blinkhealth.com>
2020-09-28 23:12:34 -0400

    add same site none to allow other domains

--- src/werkzeug/http.py
+++ src/werkzeug/http.py
@@ -1157,8 +1157,9 @@ def dump_cookie(
         expires = to_bytes(cookie_date(time() + max_age))
 
     samesite = samesite.title() if samesite else None
-    if samesite not in ("Strict", "Lax", None):
-        raise ValueError("invalid SameSite value; must be 'Strict', 'Lax' or None")
+
+    if samesite not in {"Strict", "Lax", "None"}:
+        raise ValueError("SameSite must be 'Strict', 'Lax', or 'None'.")
 
     buf = [key + b"=" + _cookie_quote(value)]
 
